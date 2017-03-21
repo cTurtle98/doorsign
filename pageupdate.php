d2540657a181042a85d6d742c5a60b3d0d5cb76a
Ciaran Farley <c@98st.us>
2017-03-21 00:31:50 -0700

    a

--- pageupdate.php
+++ pageupdate.php
@@ -9,21 +9,13 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: ".mysqli_connect_error());
 }
-$sql = "SELECT * FROM messages WHERE `active` = 1";
+$sql = "SELECT type, title, status FROM messages WHERE `active` = 1";
 $result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        array_push($resArray, $row);
-    }
-}
-
 if (mysqli_num_rows($result) > 0) {
                     while($row = mysqli_fetch_assoc($result)) {
                         array_push($resArray, $row);
                     }
                     print_r($resArray);
                    }
-
-print_r($resArray);
 
 ?>
